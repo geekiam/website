98667a0f4172bafca7b28752be81bdc46ea1eebb
Gary Woodfine <gary.woodfine@threenine.co.uk>
2019-11-10 15:45:07 +0000

    Lucia first post

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,16 +4,41 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <h1>Go Vacation Game Review</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    <p>The Go vacation   is a sports and party game . Players can explore all the places riding all the animals activities and finding  new places .</p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+
+    <h3>Improve</h3>
+    <p>While playing the game I noticed a lot of glitches, which was quite frustrating.</p>
+    <p>There should be a lot of things to do like, battles, wars, actually talk instead of speech bubbles drive aeroplanes so you can look out of the window and see the howl place. Making houses in villages. visit other people’s houses to get ideas  dying should be included. flying so they can find their house. building sky-scrapers. Choose to be on survival or creative.play hide and seek and tag because its fun.eat to get energy.</p>
+
+    <h3>Good</h3>
+    <p>Skiing is fun ,which means more people will play. driving cars is good fun and tricky. driving speed boat is good fun as well. racing people is a lot of  fun and tricky .having different vehicles to choose from  . playing with your friends. choosing different  places. having  towns.</p>
+
+    <h3>New idea</h3>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          Change the weather because it’s  always sunny and i would like it to be night sometimes.  have injuries sometimes just to make it life like. steel things to get better things for your house. fall over again make it life like. cry when you  fall over to make it real life. break a bone just to make it more fun.have headache to make it realistitick. lose  energy to make it intaning. Exercise by going to the gym and lift weights and go on
+    <p>Treadmill and cross-trainer and use the rower to. have different things to do which makes it more fun. spy on people and follow them to there house to collect things for your house . make food to eat.go to swimming pools to play. go to the cinema and theatre to get entertainment . go to sleep to get energy. Go bowling for fun. Play football and rugby to get more people to play .play the radio and music to listen to . play with the animals to make the animals and  exercise.</p>
+    <p>chat to friends to make it fun  .drink water go to restaurant for more fun. Make shows for fun and to have better things to do.  Go on water slides to make it fun and scary.go to go ape/obstacles in trees to make it tricky. Go to Trampoline park ‘s that is also part of doing exercise. phones to make calls with if you get hurt call for help.go to the shops for food.get a job like an explorer. Make your people sick sometimes to make it real istick. Make them go to the doctors because evryones sick sometimes. make them cold sometimes and sometimes make them hot otherwise it will be less fun  . have birthdays to unlock more games . Have all the special occasions just to make it fun.</p>
+
+    <p>I recommend this can be played by six year olds and above because its a very tricky game to play and some younger people might not understand it .</p>
+    <p>Written by lucia woodfine. hope you liked it .Thank you for reading.</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   </Layout>
 </template>
