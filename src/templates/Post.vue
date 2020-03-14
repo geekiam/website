3b3a84b4aa3dede34f6d763586103263dda12968
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-03-14 21:31:33 +0000

    WIP - Setting Green colour

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -3,7 +3,7 @@
         <main>
             <article class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto px-6 sm:px-12 pt-16"
                      :class="{'border-b border-grey-lighter pb-10 mb-16': !$page.post.author}">
-                <h1 class="text-3xl sm:text-4xl leading-tight font-sans mb-1 sm:mb-2" >{{ $page.post.title }}</h1>
+                <h1 class="text-green-700   text-3xl sm:text-4xl leading-tight font-sans mb-1 sm:mb-2" >{{ $page.post.title }}</h1>
                 <div :class="{'pb-10': $page.post.author || $page.post.tags}"
                      class="text-lg leading-normal text-gray-700"
                      v-html="$page.post.content" />
@@ -13,7 +13,7 @@
                     <div>
                         <g-link v-for="tag in $page.post.tags"
                                 :key="tag.id" :to="`${tag.path}/`"
-                                class="inline-block text-green-400 hover:text-white hover:bg-green-400 border border-green-400 font-sans font-bold text-xs sm:text-sm px-4 py-2 mr-4 mb-2 rounded-full transition-color transition-bg">
+                                class="inline-block text-green-700 hover:text-white hover:bg-green-500 border border-green-700 font-sans font-bold text-xs sm:text-sm px-4 py-2 mr-4 mb-2 rounded-full transition-color transition-bg">
                             <svg class="inline w-3 fill-current align-middle mr-1"
                                  xmlns="http://www.w3.org/2000/svg"
                                  viewBox="0 0 20 20"
