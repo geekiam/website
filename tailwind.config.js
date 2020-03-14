e1e972558ae456f12037495a05e52915c6b3a011
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-03-14 21:37:31 +0000

    WIP - Setting Green colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,32 @@ module.exports = {
         },
 
       },
+      fontFamily: {
+        sans: [
+          'Nunito Sans',
+          'Roboto',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+        mono: [
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace',
+        ],
+      },
       spacing: {
         '80': '20rem',
         '108': '27rem',
