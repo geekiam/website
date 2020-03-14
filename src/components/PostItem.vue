9c3289432154b9c5b502e5d784d9fb3ec8ad9e37
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-03-14 22:15:56 +0000

    WIP - Testing netlify preview

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -4,7 +4,7 @@
             <div class="py-8 sm:py-20 border-b border-gray-300">
                 <header class="text-center mb-8">
                     <time :datetime="post.datetime"
-                          class="text-gray-700 text-xs mb-2 uppercase">{{ formatPublishDate(post.datetime) }}</time>
+                          class="text-green-700 text-xs mb-2 uppercase">{{ formatPublishDate(post.datetime) }}</time>
                     <h2 class="text-green-700 text-3xl sm:text-4xl leading-tight font-sans mb-1 sm:mb-2">
                         <g-link :to="`${post.path}/`"
                                 class="text-black font-bold">{{ post.title }}</g-link>
@@ -12,7 +12,7 @@
                     <p class="text-gray-700 leading-normal text-sm sm:text-base">
                         <span v-if="post.author">by
                             <g-link :to="`${post.author.path}/`"
-                                    class="text-gray-700 capitalize border-b border-transparent hover:border-gray-400 transition-border-color"
+                                    class="text-green-700 capitalize border-b border-transparent hover:border-gray-400 transition-border-color"
                                     v-if="post.author">{{ titleCase(post.author.title) }}</g-link></span>
                         <span v-if="post.tags && post.tags.length > 0"> in <g-link :to="`${post.tags[0].path}/`"
                                                                                    class="text-gray-700 capitalize border-b border-transparent hover:border-gray-400 transition-border-color">{{ titleCase(post.tags[0].title) }}</g-link></span>
