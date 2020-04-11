434d208ec7c57dd93e62df80846a40660f01f451
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-04-11 22:25:32 +0100

    Basic web page implemented

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,15 +2,17 @@
 <layout>
     <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
         <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
-            <g-image src="~/assets/images/geek-i-am-intro.png"></g-image>
+            <g-image src="~/assets/images/geek-i-am-intro.png" width="380" ></g-image>
 
         </div>
-        <div><p class="text-4xl font-bold text-center text-green-800">Programming &amp; Coding is fun</p>
-        <p class="text-1xl text-gray-700 text-center">Learning to code</p>
-            <p class="text-1xl text-gray-700 text-center">Or doing to make a living</p>
-            <p class="text-1xl text-gray-700 text-center">   can be boring and tedious</p>
+        <div><p class="tracking-wide text-4xl font-bold text-center text-green-800">Learn to code</p>
+        <p class="tracking-wide text-2xl text-gray-700 ">Discover basics.</p>
+            <p class="tracking-wide text-2xl text-gray-700 ">Build Projects</p>
+            <p class="tracking-wide text-2xl text-gray-700 ">Develop Skills</p>
+            <p class="tracking-wide text-2xl text-gray-700">Enhance careers</p>
         </div>
     </div>
+
 </layout>
 </template>
 
