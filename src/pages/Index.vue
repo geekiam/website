c58259a2861dc5479b09dcce0832532e41f64d16
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-04-15 22:15:29 +0100

    Readme updates

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,6 @@
         <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
             <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
                 <g-image src="~/assets/images/geek-i-am-intro.png" width="380"></g-image>
-
             </div>
             <div>
                 <p class="tracking-wide text-4xl font-bold text-center text-green-800 mb-2">Learn to code</p>
--- src/pages/Tutorial.vue
+++ src/pages/Tutorial.vue
@@ -1,8 +1,10 @@
 <template>
 <layout>
+    <div class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
+        <h1 class="text-green-800 text-5xl font-bold text-center">Tutorials</h1>
+    </div>
     <div class="container-inner mx-auto flex flex-col sm:flex-row justify-between py-16">
-    <h1>Tutorial</h1>
-        <p> Coming soon ....</p>
+        <p class="text-center text-6xl">Coming Soon</p>
     </div>
 </layout>
 </template>
