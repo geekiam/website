053b7ad7f415d9ec154fb1e9544f42620857873a
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-06-29 15:36:52 +0100

    Use tailwind.config.js purge option

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,7 +72,7 @@ module.exports = {
             },
             fontSize: {
                 'xs': '.75rem',
-
+                sm: '.875rem',
                 'base': '1rem',
                 'lg': '1.125rem',
                 'xl': '1.25rem',
