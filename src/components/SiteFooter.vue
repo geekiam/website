d989723a0974a60178ad6500cda0055df7f6c3b5
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-06-30 20:58:49 +0100

    Updated footer to reduce noise

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -15,21 +15,7 @@
                                         Marketing
                                     </a>
                                 </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Analytics
-                                    </a>
-                                </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Commerce
-                                    </a>
-                                </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Insights
-                                    </a>
-                                </li>
+
                             </ul>
                         </div>
                         <div class="mt-12 md:mt-0">
@@ -42,21 +28,7 @@
                                         Pricing
                                     </a>
                                 </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Documentation
-                                    </a>
-                                </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Guides
-                                    </a>
-                                </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        API Status
-                                    </a>
-                                </li>
+
                             </ul>
                         </div>
                     </div>
@@ -72,25 +44,12 @@
                                     </a>
                                 </li>
                                 <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
+                                    <g-link class="text-base leading-6 text-gray-300 hover:text-white" to="/blog">
                                         Blog
-                                    </a>
+                                    </g-link>
                                 </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Jobs
-                                    </a>
-                                </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Press
-                                    </a>
-                                </li>
-                                <li class="mt-4">
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Partners
-                                    </a>
-                                </li>
+
+
                             </ul>
                         </div>
                         <div class="mt-12 md:mt-0">
@@ -98,12 +57,8 @@
                                 Legal
                             </h4>
                             <ul class="mt-4">
-                                <li>
-                                    <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
-                                        Claim
-                                    </a>
-                                </li>
-                                <li class="mt-4">
+
+                            <li class="mt-4">
                                     <a class="text-base leading-6 text-gray-300 hover:text-white" href="#">
                                         Privacy
                                     </a>
@@ -130,7 +85,7 @@
                                placeholder="Enter your email" required type="email"/>
                         <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
                             <button
-                                class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 transition duration-150 ease-in-out">
+                                class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-500 hover:bg-green-400 focus:outline-none focus:bg-green-400 transition duration-150 ease-in-out">
                                 Subscribe
                             </button>
                         </div>
@@ -139,24 +94,6 @@
             </div>
             <div class="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between">
                 <div class="flex md:order-2">
-                    <a class="text-gray-400 hover:text-gray-300" href="#">
-          <span class="sr-only">Facebook
-          </span>
-                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                            <path clip-rule="evenodd"
-                                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                                  fill-rule="evenodd"/>
-                        </svg>
-                    </a>
-                    <a class="ml-6 text-gray-400 hover:text-gray-300" href="#">
-          <span class="sr-only">Instagram
-          </span>
-                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                            <path clip-rule="evenodd"
-                                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                                  fill-rule="evenodd"/>
-                        </svg>
-                    </a>
                     <a class="ml-6 text-gray-400 hover:text-gray-300" href="#">
           <span class="sr-only">Twitter
           </span>
@@ -165,7 +102,7 @@
                                 d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"/>
                         </svg>
                     </a>
-                    <a class="ml-6 text-gray-400 hover:text-gray-300" href="#">
+                    <a class="ml-6 text-gray-400 hover:text-gray-300" href="https://github.com/Geek-I-Am">
           <span class="sr-only">GitHub
           </span>
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -174,15 +111,7 @@
                                   fill-rule="evenodd"/>
                         </svg>
                     </a>
-                    <a class="ml-6 text-gray-400 hover:text-gray-300" href="#">
-          <span class="sr-only">Dribbble
-          </span>
-                        <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-                            <path clip-rule="evenodd"
-                                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                                  fill-rule="evenodd"/>
-                        </svg>
-                    </a>
+
                 </div>
                 <p class="mt-8 text-base leading-6 text-gray-400 md:mt-0 md:order-1">
                     &copy; 2020 <a
@@ -192,103 +121,6 @@
             </div>
         </div>
     </div>
-
-
-    <!--<footer class="bg-green-800 text-white">
-        <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between py-8">
-            <div class="mb-8 lg:mb-0">
-                <div class="mb-3">
-                    <p>&copy Copyright {{ new Date().getFullYear() }} . All rights reserved.</p>
-                    <p>
-                        Developed by
-                        <a
-                            class="text-orange-700 border-b border-transparent hover:border-orange-400 transition-border-color"
-                            href="https://threenine.co.uk"
-                        >
-                            threenine.co.uk
-                        </a>
-                        with
-                        <a
-                            class="border-b border-transparent hover:border-gray-400 transition-border-color"
-                            href="https://gridsome.org"
-                        >
-                            Gridsome
-                        </a>
-                        &amp;
-                        <a
-                            class="border-b border-transparent hover:border-gray-400 transition-border-color"
-                            href="https://tailwindcss.com/"
-                        >
-                            Tailwind CSS
-                        </a>
-                        .
-                    </p>
-                </div>
-                <div class="mb-3">
-                    <a class="text-white hover:text-gray-400 font-normal" href="/feed.xml">RSS Feed</a>
-                    |
-                    <a class="text-white hover:text-gray-400 font-normal" href="/sitemap.xml">Sitemap</a>
-                </div>
-                <ul class="flex items-center">
-                    <li class="mr-8">
-                        <a class="text-white hover:text-gray-400" href="mailto:hello@geekiam.co.uk">
-                            <svg fill="currentColor" height="20" width="25" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M2.5 0h20A2.5 2.5 0 0 1 25 2.5v15a2.5 2.5 0 0 1-2.5 2.5h-20A2.5 2.5 0 0 1 0 17.5v-15C0 1.125 1.125 0 2.5 0zm20 4.225V2.5h-20v1.725l10 5 10-5zm0 2.8l-9.438 4.713a1.25 1.25 0 0 1-1.124 0L2.5 7.025V17.5h20V7.025z"
-                                    fill-rule="nonzero"
-                                />
-                            </svg>
-                        </a>
-                    </li>
-
-                    <li class="mr-8">
-                        <a
-                            class="text-white hover:text-gray-400"
-                            href="https://youtube.com/threenine"
-                            target="_blank"
-                        >
-                            <svg fill="currentColor" height="20" width="26" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M25.457 3.13c-.3-1.232-1.18-2.203-2.299-2.532C21.13 0 13 0 13 0S4.87 0 2.842.598c-1.119.33-2 1.3-2.299 2.531C0 5.362 0 10.02 0 10.02s0 4.658.543 6.891c.3 1.232 1.18 2.162 2.299 2.49C4.87 20 13 20 13 20s8.13 0 10.158-.598c1.119-.33 2-1.26 2.299-2.49C26 14.677 26 10.02 26 10.02s0-4.658-.543-6.89zM10.34 14.25V5.79l6.795 4.23-6.795 4.23z"
-                                    fill-rule="nonzero"
-                                />
-                            </svg>
-                        </a>
-                    </li>
-
-                    <li class="mr-8">
-                        <a
-                            class="text-white hover:text-gray-400"
-                            href="https://github.com/geekiam"
-                            target="_blank"
-                        >
-                            <svg fill="currentColor" height="19" width="20" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M10 0c1.814 0 3.487.435 5.02 1.306a9.827 9.827 0 0 1 3.639 3.542A9.33 9.33 0 0 1 20 9.734c0 2.121-.636 4.03-1.908 5.723a9.783 9.783 0 0 1-4.928 3.518c-.234.042-.408.012-.52-.09a.49.49 0 0 1-.17-.38l.006-.969c.005-.621.007-1.19.007-1.705 0-.82-.226-1.42-.677-1.8.495-.05.94-.126 1.335-.228a5.4 5.4 0 0 0 1.223-.494 3.62 3.62 0 0 0 1.055-.843c.282-.334.512-.777.69-1.33.178-.554.267-1.19.267-1.909a3.7 3.7 0 0 0-1.028-2.61c.32-.77.286-1.631-.105-2.586-.243-.076-.594-.03-1.054.14-.46.168-.86.354-1.198.557l-.495.304a9.478 9.478 0 0 0-2.5-.33c-.86 0-1.693.11-2.5.33a11.6 11.6 0 0 0-.553-.342c-.23-.135-.593-.298-1.088-.488-.494-.19-.863-.247-1.106-.171-.391.955-.426 1.816-.105 2.585A3.7 3.7 0 0 0 3.62 9.227c0 .719.089 1.352.267 1.902.178.549.406.993.683 1.33.278.339.627.622 1.048.85a5.4 5.4 0 0 0 1.224.494c.395.102.84.178 1.335.228-.338.305-.551.74-.638 1.306a2.631 2.631 0 0 1-.586.19 3.782 3.782 0 0 1-.742.063c-.287 0-.57-.09-.853-.272a2.256 2.256 0 0 1-.723-.792 2.068 2.068 0 0 0-.631-.66c-.256-.168-.471-.27-.645-.304l-.26-.038c-.182 0-.308.02-.378.057-.07.038-.09.087-.065.146.026.06.065.118.117.178.053.059.109.11.17.152l.09.063c.192.085.38.245.567.482.187.236.324.452.41.646l.13.292c.113.32.304.58.574.78.269.198.56.325.872.38.312.054.614.084.905.088.29.004.532-.01.723-.044l.299-.05c0 .32.002.694.007 1.12l.006.692a.49.49 0 0 1-.17.38c-.112.101-.286.13-.52.089a9.783 9.783 0 0 1-4.928-3.518C.636 13.763 0 11.855 0 9.734a9.33 9.33 0 0 1 1.341-4.886 9.827 9.827 0 0 1 3.64-3.542C6.512.436 8.185 0 10 0zM3.79 13.98c.025-.058-.005-.11-.092-.151-.087-.026-.143-.017-.17.025-.025.06.005.11.092.152.078.05.134.042.17-.025zm.403.432c.06-.043.052-.11-.026-.203-.087-.076-.157-.089-.209-.038-.06.042-.052.11.026.203.087.084.157.097.209.038zm.39.57c.078-.06.078-.14 0-.24-.07-.11-.143-.136-.221-.077-.078.042-.078.118 0 .228.078.11.152.14.221.089zm.547.532c.07-.067.052-.148-.052-.24-.104-.102-.19-.115-.26-.039-.078.068-.061.148.052.241.104.102.19.114.26.038zm.742.317c.026-.093-.03-.16-.169-.203-.13-.033-.213-.004-.247.09-.035.092.021.155.169.19.13.05.213.025.247-.077zm.82.064c0-.11-.073-.157-.22-.14-.14 0-.209.047-.209.14 0 .11.074.156.221.139.14 0 .209-.046.209-.14zm.756-.127c-.017-.093-.096-.131-.234-.114-.14.025-.2.088-.183.19.018.101.096.135.235.101.139-.034.2-.093.182-.177z"
-                                    fill-rule="nonzero"
-                                />
-                            </svg>
-                        </a>
-                    </li>
-
-                    <li class="mr-8">
-                        <a
-                            class="text-white hover:text-gray-400"
-                            href="https://twitter.com/geek_i_am"
-                            target="_blank"
-                        >
-                            <svg fill="currentColor" height="17" width="20" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M19.807 1.943a8.588 8.588 0 0 1-2.036 2.135 11.918 11.918 0 0 1-.465 3.854 12.085 12.085 0 0 1-1.452 3.177 12.426 12.426 0 0 1-2.318 2.691c-.897.78-1.978 1.402-3.243 1.867-1.265.464-2.618.697-4.06.697-2.27 0-4.348-.618-6.233-1.854.293.034.62.051.98.051 1.885 0 3.565-.588 5.04-1.764a3.915 3.915 0 0 1-2.363-.825 4.038 4.038 0 0 1-1.432-2.039c.276.043.532.064.766.064.36 0 .717-.047 1.068-.14A3.982 3.982 0 0 1 1.73 8.43c-.617-.754-.925-1.63-.925-2.627v-.051c.57.324 1.182.498 1.835.524a4.1 4.1 0 0 1-1.32-1.47 4.078 4.078 0 0 1-.49-1.969c0-.75.185-1.445.553-2.084a11.548 11.548 0 0 0 3.702 3.05 11.163 11.163 0 0 0 4.669 1.271c-.067-.324-.1-.639-.1-.946 0-1.142.395-2.116 1.187-2.92C11.632.402 12.589 0 13.712 0c1.173 0 2.161.435 2.966 1.304a7.878 7.878 0 0 0 2.576-.997 3.997 3.997 0 0 1-1.785 2.275 7.98 7.98 0 0 0 2.338-.639z"
-                                    fill-rule="nonzero"
-                                />
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </footer>-->
 </template>
 
 <script>
