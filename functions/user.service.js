f15eb33870a2a4f6fee0efebbd86826db6d25602
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-11-01 18:18:24 +0000

    Update content some minor layout tweaks

---
+++ functions/user.service.js
@@ -0,0 +1,47 @@
+const axios = require('axios')
+const API_ENDPOINT = 'https://api.github.com'
+
+exports.handler = (event, context, callback) => {
+    const userName = event.queryStringParameters.name
+
+    const send = (body) => {
+        callback(null, {
+            statusCode: 200,
+            body: body,
+        })
+    }
+    const getUserDetail = () => {
+        axios({
+            method: 'GET',
+            url: `${API_ENDPOINT}/users/${userName}`,
+            headers: {
+                Authorization: `Bearer ${process.env.GRIDSOME_GITHUB_TOKEN}`,
+            },
+        })
+            .then((res) => send(res.data))
+            .catch((err) => send(err))
+    }
+
+    // Make sure method is GET
+    if (event.httpMethod === 'GET') {
+        // Run
+        getUserDetail()
+    }
+}
+
+/*exports.handler = async (event, context) => {
+    const userName = event.queryStringParameters.name || 'Worldn'
+
+    return fetch(`${API_ENDPOINT}/users/${userName}`, {
+        headers: {
+            Accept: 'application/json',
+            Authorization: `token ${process.env.GRIDSOME_GITHUB_TOKEN}`,
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => ({
+            statusCode: 200,
+            body: data,
+        }))
+        .catch((error) => ({ statusCode: 422, body: String(error) }))
+}*/
