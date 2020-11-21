55a10b4f968f51583aefefb8cd8da5723ac89607
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-11-21 19:54:07 +0000

    Fixing Mobile View breaking

--- src/layouts/PostLayout.vue
+++ src/layouts/PostLayout.vue
@@ -1,7 +1,5 @@
 <template>
-    <div
-        class="flex flex-grow w-full max-w-6xl mx-auto xl:px-2 lg:flex mt-10 p-4"
-    >
+    <div class="w-full mx-w-8xl mx-auto">
         <div class="lg:flex">
             <div
                 id="sidebar"
@@ -9,43 +7,20 @@
             >
                 <slot name="left-side-bar" />
             </div>
-            <div
-                class="lg:min-w-2 lg:flex-7 items-center overflow-x-auto bg-white lg:min-w-2 lg:flex-7 p-3"
-            >
-                <slot name="post-content" />
-            </div>
-            <div
-                class="lg:pr-1 lg:flex-shrink-0 lg:border-l lg:border-gray-200"
-            >
-                <div class="pl-3 lg:w-64">
-                    <slot name="right-side-bar" />
+            <div class="flex w-full">
+                <div class="min-w-0 flex-auto px-4 sm:px-6 xl:px-8 bg-white">
+                    <slot name="post-content" />
+                </div>
+                <div
+                    class="hidden xl:text-sm xl:block flex-none w-64 pl-8 mr-8"
+                >
+                    <div class="pl-3 lg:w-64">
+                        <slot name="right-side-bar" />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- <div class="flex-grow w-full max-w-6xl mx-auto xl:px-2 lg:flex">
-        <div
-            class="flex-1 min-w-1 xl:flex-1 bg-gray-100 xl:flex-shrink-1 xl:w-10"
-        >
-            <slot name="left-side-bar" />
-        </div>
-
-        <div
-            class="bg-white lg:min-w-2 lg:flex-7 pl-6 pr-2 pt-4 pb-1 border-b border-t border-gray-200 sm:pl-6 lg:pl-8 xl:pl-6 xl:pt-6 xl:border-t-0"
-        >
-            <section>
-                <slot name="post-content" />
-            </section>
-        </div>
-
-        <div
-            class="bg-gray-100 pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0"
-        >
-            <div class="pl-6 lg:w-80">
-                <slot name="right-side-bar" />
-            </div>
-        </div>
-    </div>-->
 </template>
 
 <script>
