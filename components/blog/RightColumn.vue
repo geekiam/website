f42accaba59af761e5a5a13b8b09db8faac1aa61
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-01-20 23:07:02 +0000

    Left and Right Columns

---
+++ components/blog/RightColumn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pl-6 lg:w-80">
+    <div class="pt-6 pb-2">
+      <h2 class="text-sm font-semibold dark:text-gray-100">Right Column</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RightColumn',
+}
+</script>
