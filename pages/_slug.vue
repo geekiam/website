b3506396b48eb5ffc9b3932e3c1b4b71dbe70f76
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-01-20 23:31:09 +0000

    Content Page Fixed

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,14 +1,42 @@
 <template>
-  <main class="sm:h-full flex-1 flex flex-col min-h-0 min-w-0 overflow-auto">
-    <nuxt-content
-      class="prose sm:prose-sm lg:prose-lg dark:text-gray-100 text-gray-700 pb-10"
-      :document="post"
-    />
-  </main>
+  <!-- 3 column wrapper -->
+  <div class="flex-grow w-full max-w-7xl mx-auto xl:px-8 lg:flex">
+    <!-- Left sidebar & main wrapper -->
+    <div class="flex-1 min-w-0 bg-gray-100 dark:bg-gray-900 xl:flex">
+      <!-- Account profile -->
+      <div class="xl:flex-shrink-0 xl:w-32 xl:border-r lg:border-gray-500">
+        <div class="pl-4 pr-6 py-6 sm:pl-6 lg:pl-8 xl:pl-0">
+          <div class="flex items-center justify-between">
+            <div class="flex-1 space-y-8">
+              <p class="text-gray-100">Left Column</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Projects List -->
+      <div class="bg-white dark:bg-gray-900 lg:min-w-0 lg:flex-1">
+        <div class="pl-4 pr-6">
+          <nuxt-content
+            class="prose sm:prose-sm lg:prose-lg dark:text-gray-100 text-gray-700 pb-10 mx-auto"
+            :document="post"
+          />
+        </div>
+      </div>
+    </div>
+    <!-- Activity feed -->
+    <div
+      class="bg-gray-100 dark:bg-gray-900 pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0 lg:border-l lg:border-gray-500 xl:pr-0"
+    >
+      <right-column></right-column>
+    </div>
+  </div>
 </template>
 
 <script>
+import RightColumn from '../components/blog/RightColumn'
 export default {
+  components: { RightColumn },
   layout: 'home',
   async asyncData({ $content, params }) {
     const post = await $content('posts', params.slug).fetch()
