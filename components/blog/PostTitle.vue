0097d0290bf06eb1719928bdf7542a0d60338958
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-01-22 12:24:39 +0000

    Formatting dates with moment

--- components/blog/PostTitle.vue
+++ components/blog/PostTitle.vue
@@ -18,7 +18,7 @@
 
       <span
         class="mr-4 text-gray-500 dark:text-gray-100 ml-1"
-        v-html="$moment(post.date)"
+        v-html="$moment(post.date).format('d MMM yyyy')"
       ></span>
       <svg
         class="h-4 w-4 mr-1 ml-4 fill-current"
