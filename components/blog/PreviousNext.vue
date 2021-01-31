8bbfca765cc0626d307796075d724ff0be69171f
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-01-31 20:12:53 +0000

    Tinkering with Placement in Prev/Next

--- components/blog/PreviousNext.vue
+++ components/blog/PreviousNext.vue
@@ -2,33 +2,29 @@
   <div aria-label="Progress">
     <ol class="space-y-4 md:flex md:space-y-0 md:space-x-8">
       <li class="md:flex-1">
-        <div class="content-start">
-          <!-- Previous Step -->
-          <nuxt-link
-            :to="previous.url"
-            class="group pl-4 py-2 flex flex-col border-l-4 border-green-800 hover:border-green-500 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+        <!-- Previous Step -->
+        <nuxt-link
+          :to="previous.url"
+          class="group pl-4 py-2 flex flex-col border-l-4 border-green-800 hover:border-green-500 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+        >
+          <div
+            class="text-xs text-green-800 font-semibold group-hover:text-green-500 grid justify-items-start"
           >
-            <span
-              class="text-xs text-green-800 font-semibold uppercase group-hover:text-green-500"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="h-4 w-4 fill-current mr-2"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="h-4 w-4 fill-current mr-2"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z"
-                  clip-rule="evenodd"
-                /></svg
-            ></span>
-            <span
-              class="text-sm font-medium text-green-800"
-              v-text="previous.title"
-            ></span>
-          </nuxt-link>
-        </div>
+              <path
+                fill-rule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <span v-html="previous.title"></span>
+          </div>
+        </nuxt-link>
       </li>
       <li class="md:flex-1">
         <!-- Next Step -->
@@ -51,12 +47,8 @@
                 clip-rule="evenodd"
               />
             </svg>
-            <span class="" v-html="next.title"></span>
+            <span v-html="next.title"></span>
           </div>
-          <!--            <span-->
-          <!--              class="text-sm font-medium text-green-800"-->
-          <!--              v-text="next.title"-->
-          <!--            ></span>-->
         </nuxt-link>
       </li>
     </ol>
