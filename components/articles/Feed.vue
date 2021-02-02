1d4cdd1c7b746f0cada9236cf509f86636c8b360
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-02-02 16:40:48 +0000

    fixed bug where feed was not passing section to card component

--- components/articles/Feed.vue
+++ components/articles/Feed.vue
@@ -5,15 +5,7 @@
       :key="post.slug"
       class="relative pl-4 pr-6 py-5 hover:bg-gray-50 sm:py-6 sm:pl-6 lg:pl-8 xl:pl-6"
     >
-      <div class="flex items-center justify-between space-x-4">
-        <div class="min-w-0 space-y-3">
-          <div class="flex items-center space-x-3">
-            <span class="block">
-              <card :post="post" :section="section" />
-            </span>
-          </div>
-        </div>
-      </div>
+      <card :post="post" :section="section" />
     </li>
   </ul>
 </template>
