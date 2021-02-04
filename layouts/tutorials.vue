f4ac86f2d7b4fb91f05cf52723f7bfbef24314d9
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-02-04 11:49:46 +0000

    WIP - Checking repo organisation move

---
+++ layouts/tutorials.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-100 dark:bg-gray-900">
+    <review-header></review-header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TutorialHome',
+}
+</script>
--- pages/reviews/index.vue
+++ pages/reviews/index.vue
@@ -4,6 +4,6 @@
 
 <script>
 export default {
-  layout: 'ReviewsHome',
+  layout: 'tutorials',
 }
 </script>
