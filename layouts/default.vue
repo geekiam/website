99bf33d92225843538769189eb2441c05b897750
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-05-25 20:51:19 +0100

    added fathom plugin

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,32 @@
 <script lang="ts" setup>
-
+useHead({
+  title: 'Geekiam',
+  meta: [
+    {
+      name: 'description',
+      content: 'Discover, Organise, Share and Promote learning in communities of interest',
+    },
+    { name: 'og:title', content: 'Geekiam' },
+    {
+      name: 'og:description',
+      content: 'Discover, Organise, Share and Promote learning in communities of interest',
+    },
+    { name: 'og:image', content: 'https://geekiam.io/geekiam.png' },
+    { name: 'og:url', content: 'https://geekiam.io' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:site', content: '@geekiamio' },
+    { name: 'twitter:creator', content: '@geekiamio' },
+    { name: 'twitter:title', content: 'Geekiam' },
+    {
+      name: 'twitter:description',
+      content: 'Discover, Organise, Share and Promote learning in communities of interest',
+    },
+    { name: 'twitter:image', content: 'https://geekiam.io/geekiam.png' },
+  ],
+  script: [
+    { src:'' },
+  ]
+})
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import {isProduction} from "std-env";
 export default defineNuxtConfig({
-    app:{
-        head: {
-            script: [
-                { src: 'https://cdn.usefathom.com/script.js', defer: true, 'data-site': process.env.fathom_analytics_id, 'data-spa': 'auto', 'data-excluded-domains':'localhost' }
-            ],
-        }
-    },
-    modules: ['@nuxt/content', '@nuxtjs/tailwindcss'  ],
+     modules: ['@nuxt/content', '@nuxtjs/tailwindcss'  ],
     tailwindcss: {
         cssPath: '~/assets/css/tailwind.css',
         configPath: 'tailwind.config',
